c8a1a0a6d716e0448e8e4f838bc89730726cea90
Paul M. Jones <pmjones88@gmail.com>
2015-01-19 15:22:16 -0600

    remove abstract methods to soothe 5.3, at least on windows; fixes #15

--- src/AbstractSchema.php
+++ src/AbstractSchema.php
@@ -74,31 +74,6 @@ abstract class AbstractSchema implements SchemaInterface
 
     /**
      *
-     * Returns a list of tables in the database.
-     *
-     * @param string $schema Optionally, pass a schema name to get the list
-     * of tables in this schema.
-     *
-     * @return string[] The list of table-names in the database.
-     *
-     */
-    abstract public function fetchTableList($schema = null);
-
-    /**
-     *
-     * Returns an array of columns in a table.
-     *
-     * @param string $spec Return the columns in this table. This may be just
-     * a `table` name, or a `schema.table` name.
-     *
-     * @return Column[] An associative array where the key is the column name
-     * and the value is a Column object.
-     *
-     */
-    abstract public function fetchTableCols($spec);
-
-    /**
-     *
      * Returns the column factory object.
      *
      * @return ColumnFactory

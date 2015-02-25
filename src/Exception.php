de98cbc94f270c2fabfdbc1b573e4a2336567f3e
Paul M. Jones <pmjones88@gmail.com>
2015-02-25 15:18:22 -0600

    interim commit

---
+++ src/Exception.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ * This file is part of Aura for PHP.
+ *
+ * @package Aura.SqlSchema
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ *
+ */
+namespace Aura\SqlSchema;
+
+use Exception as PhpException;
+
+/**
+ *
+ * Generic package exception.
+ *
+ */
+class Exception extends PhpException
+{
+}
